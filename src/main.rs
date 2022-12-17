841f609238c91e84eea60f8285d6c30a70783d82
berracode <juanguillermoalarcon@gmail.com>
2022-12-17 01:44:07 -0500

    unit test are added

--- src/main.rs
+++ src/main.rs
@@ -13,3 +13,36 @@ fn main() {
     println!("query {}", params.query);
 
 }
+
+#[test]
+#[should_panic]
+fn params_not_found() {
+    let args: Vec<String> = env::args().collect();
+    println!("{:?}",args);
+    let params_test = Params::new(&args);
+
+    mini_grep::run(&params_test);
+}
+
+#[test]
+#[should_panic]
+fn file_does_not_exist(){
+    let params_test = Params{
+        filename: ".\\mini_grep.imel".to_string(),
+        query: "so".to_string()
+    };
+
+    mini_grep::run(&params_test);
+}
+
+
+#[test]
+fn file_exist(){
+    let params_test = Params{
+        filename: ".\\mini_grep.iml".to_string(),
+        query: "so".to_string()
+    };
+
+    mini_grep::run(&params_test);
+}
+
